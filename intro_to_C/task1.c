6953c8428508b45a137d5c2289c2acbc801a92d5
JermaineMofe <holadekingjnr@gmail.com>
2022-09-29 03:48:42 -0700

    first c pratice

---
+++ intro_to_C/task1.c
@@ -0,0 +1,15 @@
+/* a program that prints out the numeric values of characters Z and z */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char Z;
+	char z;
+	Z = 'Z';
+
+	z = 'z';
+
+	printf("the numerical values are:\nz=%d\nZ=%d\n", z, Z);
+	return (0);
+}
